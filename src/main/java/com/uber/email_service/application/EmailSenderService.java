c4b3cd54fc24e33e88ada506867da1c73479d814
Marconi de Carvalho Alves Junior <marconi.carvalho.bcc@gmail.com>
2024-08-03 14:49:44 -0300

    created exceptions

--- src/main/java/com/uber/email_service/application/EmailSenderService.java
+++ src/main/java/com/uber/email_service/application/EmailSenderService.java
@@ -3,7 +3,9 @@ package com.uber.email_service.application;
 import com.uber.email_service.adapters.EmailSenderGatway;
 import com.uber.email_service.core.EmailSenderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailSenderService implements EmailSenderUseCase {
 
     private final EmailSenderGatway emailSenderGatway;
